1eb8247d82406c9e45100e970ac9c10efa6ccb3b
Joaquin Domenech <joackodomenech@gmail.com>
2024-11-10 20:20:56 -0300

    vista main menu y pausa

--- ui/game_screen.gd
+++ ui/game_screen.gd
@@ -3,10 +3,14 @@ extends CanvasLayer
 @onready var collectible_label = $MarginContainer/VBoxContainer/HBoxContainer/CollectibleLabel
 
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	CollectiblesManager.on_collectible_award_received.connect(on_collectible_award_received)
 
 
 func on_collectible_award_received(total_award : int):
 	collectible_label.text = str(total_award)
+
+
+func _on_pause_texture_button_pressed():
+	GameManager.pause_game()
